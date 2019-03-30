ef39626f3d0d91af023a60ffd79f51899208274e
adolfo <buylla.adolfo@hotmail.com>
2019-03-30 12:20:36 +0100

    code format

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -2,38 +2,30 @@ const Todos = require('../models/todoModel');
 const bodyParser = require('body-parser');
 
 module.exports = function (app) {
-
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({
         extended: true
     }));
 
     app.get('/api/todos/:uname', function (req, res) {
-
         Todos.find({
             username: req.params.uname
         }, function (err, todos) {
             if (err) throw err;
-
             res.send(todos);
         });
-
     });
 
     app.get('/api/todo/:id', function (req, res) {
-
         Todos.findById({
             _id: req.params.id
         }, function (err, todo) {
             if (err) throw err;
-
             res.send(todo);
         });
-
     });
 
     app.post('/api/todo', function (req, res) {
-
         if (req.body.id) {
             Todos.findByIdAndUpdate(req.body.id, {
                 todo: req.body.todo,
@@ -45,8 +37,7 @@ module.exports = function (app) {
                 res.send('Success');
             });
         } else {
-
-            var newTodo = Todos({
+            let newTodo = Todos({
                 username: 'test',
                 todo: req.body.todo,
                 isDone: req.body.isDone,
@@ -56,18 +47,14 @@ module.exports = function (app) {
                 if (err) throw err;
                 res.send('Success');
             });
-
         }
-
     });
 
     app.delete('/api/todo', function (req, res) {
-
         Todos.findByIdAndRemove(req.body.id, function (err) {
             if (err) throw err;
             res.send('Success');
         })
-
     });
 
 }
